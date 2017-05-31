569d73f095c4bd0efb41c304a7196713b8dd614e
lwj_wuwang <lwj_wuwang@sina.com>
2017-05-31 11:03:35 +0800

    test

--- test.php
+++ test.php
@@ -6,6 +6,6 @@
  * Time: 10:57
  */
 
-$conn = mysql_connect("localhost","root","root");
+$conn = mysql_connect("localhost","root","xqqdzswsd");
 $res = mysql_select_db("onenet_demo");
 var_dump($res);
