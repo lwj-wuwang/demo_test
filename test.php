98d86265e698f245fe818163335d510605c323fc
lwj_wuwang <lwj_wuwang@sina.com>
2017-05-31 11:31:48 +0800

    test

--- test.php
+++ test.php
@@ -5,7 +5,8 @@
  * Date: 2017/5/31
  * Time: 10:57
  */
+require_once './config.php';
 
-$conn = mysql_connect("localhost","root","xqqdzswasd");
-$res = mysql_select_db("onenet_demo");
+$conn = mysql_connect(DB_HOST,DB_USERNAME,DB_PASSWORD);
+$res = mysql_select_db(DB_NAME);
 var_dump($res);
