5ff41fb5a03d84aca194ee02a5df6158e8987e96
lvwenjing <lvwenjing@chinasoftinc.com>
2017-07-11 10:29:13 +0800

    test

--- api_test/test.php
+++ api_test/test.php
@@ -5,7 +5,9 @@
  * Date: 2017/7/11
  * Time: 10:19
  */
-
+header("Content-type: text/html; charset=utf-8");
+date_default_timezone_set('Asia/Chongqing');
+require_once "./Onepush/util.php";
 
 $raw_input = file_get_contents('php://input');
 $resolved_body = Util::resolveBody($raw_input);
