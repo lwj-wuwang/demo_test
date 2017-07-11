76b21c6052cbc0eff14a2aa64406e8d5d451331c
lvwenjing <lvwenjing@chinasoftinc.com>
2017-07-11 11:14:40 +0800

    test

--- api_test/test.php
+++ api_test/test.php
@@ -9,10 +9,10 @@ header("Content-type: text/html; charset=utf-8");
 date_default_timezone_set('Asia/Chongqing');
 require_once "./Onepush/util.php";
 
-
+file_put_contents('./data_1.txt',print_r($GLOBALS['HTTP_RAW_POST_DATA'], TRUE),FILE_APPEND);
 //file_put_contents("./data.txt", print_r($GLOBALS['HTTP_RAW_POST_DATA'], TRUE), FILE_APPEND);die;
 $raw_input = json_decode($GLOBALS['HTTP_RAW_POST_DATA'],true);
-file_put_contents('./data_2.txt',print_r($resolved_body),FILE_APPEND);
+file_put_contents('./data_2.txt',print_r($resolved_body,true),FILE_APPEND);
 //$raw_input = file_get_contents('php://input');
 $resolved_body = Util::resolveBody($raw_input);
 file_put_contents('./data.txt',print_r($resolved_body),FILE_APPEND);
