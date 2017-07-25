6167866f34ea5b9ca5616680d4567b810de40abf
lvwenjing <lvwenjing@chinasoftinc.com>
2017-07-25 16:11:04 +0800

    test

--- datapush.php
+++ datapush.php
@@ -16,7 +16,7 @@ $rowsArr = $tabeClass->getList('dev_error','*','','id desc');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>推送告警</title>
     <script type="text/javascript" src="jquery.js"></script>
     <script type="text/javascript" src="jquery-1.6.2.min.js"></script>
     <style type="text/css">
